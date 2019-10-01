9ca46854742da1d5231d23e5020e151e789257c5
Mujiburrohman <mujiburr741@gmail.com>
2019-10-01 09:24:44 +0700

    woakoakw
    awokaowkawoka

--- app/src/main/java/com/alfianyusufabdullah/chatyuk/ChatExtensions.kt
+++ app/src/main/java/com/alfianyusufabdullah/chatyuk/ChatExtensions.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.snackbar.Snackbar
 
+//akwokoawkowak
 fun AppCompatActivity.replaceFragment(resources: Int, fragment: Fragment) {
     supportFragmentManager
             .beginTransaction()
